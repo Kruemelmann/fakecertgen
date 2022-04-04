f89821f969e86798447d9645aacf374236f326c7
Kruemelmann <kruemelmannlp@gmail.com>
2022-04-04 18:13:04 +0200

    init cert on startup

--- cmd/fake-certgen/main.go
+++ cmd/fake-certgen/main.go
@@ -5,17 +5,25 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/kruemelmann/fake-certgen/pkg/cert"
 	"github.com/kruemelmann/fake-certgen/pkg/handlers"
 )
 
 func main() {
-	r := mux.NewRouter()
+	log.Printf("Starting fake-certgen\n")
 
+	//init CA
+	cert.GetCA()
+
+	//init Certificate
+	cert.GetCertificate()
+
+	//Webserver setup
+	r := mux.NewRouter()
 	// Register all handlers with the corresponding routes
 	r.HandleFunc("/", handlers.HomeHandler)
 	r.HandleFunc("/cert", handlers.CertHandler)
 	r.HandleFunc("/renew", handlers.RenewHandler)
-
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":9000", r))
 }
